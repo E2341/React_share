9512c844e3997e91b19b054135b06280ad88514a
Hüseyin <huseyinozer3170@gmail.com>
2021-04-06 22:30:29 +0200

    Privat route added.

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,18 +1,23 @@
+import {useContext} from "react";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Main from "../pages/Main";
 import SignUp from "../pages/SignUp";
 import UserDetail from "../pages/UserDetail"
 import Navbar from "../components/Navbar";
 import SignIn from "../pages/SignIn";
+import { FirebaseAuthContext } from '../Context/AuthContext';
+
 
 function AppRouter() {
+  const { currentUser } = useContext(FirebaseAuthContext);
+
     return (
       <Router>
         <Navbar />
         <Switch>
           <Route exact path="/register" component={SignUp}/>
           <Route exact path="/login" component={SignIn}/>
-          <Route exact path="/user/:id" component={UserDetail}/>
+          <Route exact path="/user/:id" component={currentUser ? UserDetail : SignIn}/>
           <Route path="/" component={Main}/>
         </Switch>
       </Router>
